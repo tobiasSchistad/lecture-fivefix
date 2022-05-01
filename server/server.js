23eff57f21ee041b7ab9ecfb8c0bd5b7640f4e7c
tobias.Schistad <tobias151299@gmail.com>
2022-05-01 15:36:03 +0200

    Add prettier and husky

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ app.post("/login", (req, res) => {
   res.sendStatus(401);
 });
 
+app.use(express.static("public"));
+
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Started on http://localhost:${server.address().port}`);
 });
