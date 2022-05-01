ece23c8f0dfb3e14f8d032f126c3524fcdf9572f
tobias.Schistad <tobias151299@gmail.com>
2022-05-01 16:30:20 +0200

    Middleware fix

--- server/server.js
+++ server/server.js
@@ -1,15 +1,23 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 
-app.get("/login", (req, res) => {
+app.get("/api/login", (req, res) => {
   res.json({ username: "Noen andre Persson" });
 });
-app.post("/login", (req, res) => {
+app.post("/api/login", (req, res) => {
   res.sendStatus(401);
 });
 
 app.use(express.static("../client/dist"));
+app.use((req, res, next) => {
+  if (req.method === "GET" && !req.path.startsWith("/api")) {
+    res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    next();
+  }
+});
 
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Started on http://localhost:${server.address().port}`);
