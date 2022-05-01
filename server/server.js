ee279bd141cc0676be70f5491174afeedb087f82
tobias.Schistad <tobias151299@gmail.com>
2022-05-01 14:02:28 +0200

    Use port from environment

--- server/server.js
+++ server/server.js
@@ -9,6 +9,6 @@ app.post("/login", (req,res) =>{
     res.sendStatus(401);
 })
 
-app.listen(process.env.PORT || 3000, () =>{
+const server = app.listen(process.env.PORT || 3000, () =>{
     console.log(`Started on http://localhost:${server.address().port}`)
 });
