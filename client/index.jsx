764ef7891f7df9e041e075e2d24dce568d0271db
tobias.Schistad <tobias151299@gmail.com>
2022-05-01 16:15:30 +0200

    Tell Heroku how to build client - update

--- client/index.jsx
+++ client/index.jsx
@@ -1,7 +1,45 @@
 import React from "react";
 import ReactDom from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-ReactDom.render(
-  <h1>Hello React with Express</h1>,
-  document.getElementById("app")
-);
+function FrontPage() {
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <ul>
+        <li>
+          <Link to={"/login"}>Login</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+function Login() {
+  return (
+    <form>
+      <h1>Login</h1>
+      <div>
+        Username <input />
+      </div>
+      <div>
+        Password <input type={"password"} />
+      </div>
+      <div>
+        <button>Login</button>
+      </div>
+    </form>
+  );
+}
+
+function Application() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<FrontPage />} />
+        <Route path={"/login"} element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+ReactDom.render(<Application />, document.getElementById("app"));
